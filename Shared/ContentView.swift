37e445e46c773915b36ddd7937ee622d1250e777
Onemo Kang <onemokang@outlook.com>
2022-09-17 21:33:47 -0400

    initial commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Onemo Kang on 9/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+            }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/PolyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolyApp.swift
+//  Shared
+//
+//  Created by Onemo Kang on 9/17/22.
+//
+
+import SwiftUI
+
+@main
+struct PolyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
